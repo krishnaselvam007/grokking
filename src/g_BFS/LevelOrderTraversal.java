977c6c7e7b040bf7fa931165a51756ec78636d8e
krishnaselvam007 <krishnaselvam007@outlook.in>
2022-02-25 23:17:49 +0530

    Level Order Traversal BFS

--- src/g_BFS/LevelOrderTraversal.java
+++ src/g_BFS/LevelOrderTraversal.java
@@ -1,5 +1,11 @@
 package g_BFS;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LevelOrderTraversal {
 
 	/*
@@ -9,8 +15,37 @@ public class LevelOrderTraversal {
 	 */
 	
 	public static void main(String[] args) {
+		TreeNode root = new TreeNode(12);
+		root.left = new TreeNode(7);
+		root.right = new TreeNode(1);
+		root.left.left = new TreeNode(9);
+		root.right.left = new TreeNode(10);
+		root.right.right = new TreeNode(5);
 		
+		List<List<Integer>> result = traverse(root);
+		System.out.println(result);
 
+	}
+
+	private static List<List<Integer>> traverse(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (root == null) return result;
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			System.out.println("size :: "+size);
+			List<Integer> currentLevel = new ArrayList<Integer>(size);
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				currentLevel.add(node.val);
+				System.out.println("Current Level :: "+currentLevel);
+				if (node.left != null) queue.offer(node.left);
+				if (node.right != null) queue.offer(node.right);
+			}
+			result.add(currentLevel);
+		}
+		return result;
 	}
 
 }
