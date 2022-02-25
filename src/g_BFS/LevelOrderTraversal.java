0714d9686b477935483ca1a39c8adcfe054f4773
krishnaselvam007 <krishnaselvam007@outlook.in>
2022-02-25 22:34:05 +0530

    Create LevelOrderTraversal.java

---
+++ src/g_BFS/LevelOrderTraversal.java
@@ -0,0 +1,25 @@
+package g_BFS;
+
+public class LevelOrderTraversal {
+
+	/*
+	 * Problem Statement # Given a binary tree, populate an array to represent its
+	 * level-by-level traversal. You should populate the values of all nodes of each
+	 * level from left to right in separate sub-arrays.
+	 */
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	public TreeNode(int x) {
+		val = x;
+	}
+}
